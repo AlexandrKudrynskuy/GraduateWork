7633ca336dc373913dc474dd3e2b677eab7852ae
OlexandrVorobchak <eclipsesaers@gmail.com>
2023-12-28 00:30:12 +0200

    AddIdentity

---
+++ .NetPart/TradeAssistant/DLL/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task  CreateAsync(T entity);
+        Task UpdateAsync(int id, T newEntity );
+        Task DeleteAsync(int id);
+        Task<IEnumerable<T>> GetFromConditionAsync(Expression<Func<T, bool>> condition);
+    }
+}
---
+++ .NetPart/TradeAssistant/TradeAssistant/Infrastructure/ConfigurationApp.cs
@@ -0,0 +1,61 @@
+﻿using DLL.Context;
+using Domain.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace TradeAssistant.Infrastructure
+{
+    public class ConfigurationApp
+    {
+        private WebApplicationBuilder builder;
+
+        public ConfigurationApp(WebApplicationBuilder builder)
+        {
+            this.builder = builder;
+        }
+
+        public void ConfigureService()
+        {
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<TradeAssistantContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("myConStringn")));
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<TradeAssistantContext>().AddDefaultTokenProviders();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
+
+            builder.Services.AddAuthentication(op =>
+            {
+                op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                op.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }
+            ).AddJwtBearer(op =>
+       {
+           op.SaveToken = true;
+           op.RequireHttpsMetadata = false;
+           op.TokenValidationParameters = new TokenValidationParameters()
+           {
+               ValidateIssuer = true,
+               ValidateAudience = true,
+               ValidAudience = builder.Configuration["JWT:ValidAudience"],
+               ValidIssuer = builder.Configuration["JWT:ValidIssuier"],
+               IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+
+
+           };
+       });
+
+            builder.Services.AddApplicationInsightsTelemetry();
+        }
+    }
+}
