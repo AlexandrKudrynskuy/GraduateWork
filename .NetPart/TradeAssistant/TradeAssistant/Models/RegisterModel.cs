9fb93dbf1b915cfc9094df2cc79fca96940d0943
OlexandrVorobchak <eclipsesaers@gmail.com>
2023-12-28 00:30:37 +0200

    Squashed commit of the following:
    commit 7633ca336dc373913dc474dd3e2b677eab7852ae
    Author: OlexandrVorobchak <eclipsesaers@gmail.com>
    Date:   Thu Dec 28 00:30:12 2023 +0200
    
        AddIdentity

---
+++ .NetPart/TradeAssistant/TradeAssistant/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TradeAssistant.Models
+{
+    public class RegisterModel
+    {
+        
+            [Required(ErrorMessage = "User Name is required")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Password is required")]
+            public string Password { get; set; }
+
+            [Required(ErrorMessage = "EMail is required")]
+            public string Email { get; set; }
+
+        
+    }
+}
--- .NetPart/TradeAssistant/TradeAssistant/Program.cs
+++ .NetPart/TradeAssistant/TradeAssistant/Program.cs
@@ -1,13 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using TradeAssistant.Infrastructure;
 
+var builder = WebApplication.CreateBuilder(args);
+var configurationApp = new ConfigurationApp(builder);
+configurationApp.ConfigureService();
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,9 +20,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+ app.UseAuthentication();
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
